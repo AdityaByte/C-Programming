63ceb84e88f517ef592ce405976ee53f1d75864e
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2024-10-27 19:06:42 +0530

    Hello world program in c

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+// In this program we are printing out hello world on screen its the basic program with some advanced things
+// Firstly we need to add the header files that have been preprocessed by the processor
+
+#include<stdio.h> // Header file for input / output
+
+// Main function defination
+// Entry point for execution
+int main(){
+  printf("Hello world..."); // printf is the function in the header file stdio.h that prints output to screen.
+  return 0;
+}
